61ec259b0ea240d2f5476244dba5483ad252f772
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-02-13 23:48:07 +0100

    fix

--- dashboard-fragments/dashboard_sidebar.php
+++ dashboard-fragments/dashboard_sidebar.php
@@ -6,14 +6,14 @@
                     </div>
                     <div class="sidebar_menu">
                         <ul>
-                            <li class="active_menu"><a href="#"> <i class="fas fa-chart-line"></i> Dashboard </a>
+                            <li class="dashboard-menu"><a href="#"> <i class="fas fa-chart-line"></i> Dashboard </a>
                             </li>
-                            <li><a href="students.php"> <i class="fa fa-user-graduate"></i> Students </a></li>
-                            <li><a href="tutors.php"> <i class="fas fa-chalkboard-teacher"></i> Tutors </a></li>
-                            <li><a href="admins.php"> <i class="fas fa-chalkboard-teacher"></i>
+                            <li class="students-menu"><a href="students.php"> <i class="fa fa-user-graduate"></i> Students </a></li>
+                            <li class="tutors-menu"><a href="tutors.php"> <i class="fas fa-chalkboard-teacher"></i> Tutors </a></li>
+                            <li class="admins-menu"><a href="admins.php"> <i class="fas fa-chalkboard-teacher"></i>
                                 Admins </a></li>
-                            <li><a href="profile.php"> <i class="fa fa-cogs"></i> Profile </a></li>
-                            <li><a href="change-password.php"> <i class="fa fa-key"></i> Change Password
+                            <li class="profile-menu"><a href="profile.php"> <i class="fa fa-cogs"></i> Profile </a></li>
+                            <li class="change-password-menu"><a href="change-password.php"> <i class="fa fa-key"></i> Change Password
                                 </a></li>
                         </ul>
                     </div>
