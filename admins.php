948e577e240fa8e90e5bec7af32abdef3924c4a0
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-02-14 00:31:06 +0100

    refactor

--- admins.php
+++ admins.php
@@ -9,8 +9,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <link rel="stylesheet" href="css/dashboard.css">
-    <link rel="stylesheet" href="css/students.css">
     <link rel="stylesheet" href="css/admins.css">
 
 </head>
